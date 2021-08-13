42696f7f6393f17228f9bedd8f39a80fbb60f395
John <john497dit@hotmail.com>
2021-08-12 22:47:38 -0500

    update

--- sat.js
+++ sat.js
@@ -1,29 +1,53 @@
-function formFillOut() {
-  // update Paragraphs
-  var getAssignmentName = document.getElementById("assignmentName");
-  var assignmentNameOutput = document.getElementById("name");
-  assignmentNameOutput.innerHTML =
-    "Assignment Name: " + getAssignmentName.value;
-
-  var getDate = document.getElementById("date");
-  var dateOutput = document.getElementById("dateOutput");
-  dateOutput.innerHTML = "Date: " + getDate.value;
-
-  // update Table
-  var myTable = document.getElementById("assignments");
-  var row = 1;
-  var newRow = myTable.insertRow(row);
-
-  var cell1 = newRow.insertCell(0);
-  var cell2 = newRow.insertCell(1);
-  var cell3 = newRow.insertCell(2);
-  cell1.innerHTML = getAssignmentName.value;
-  cell2.innerHTML = getDate.value;
-  cell3.innerHTML = "<input type='checkbox' id='check'>";
-}
-function clearInputs() {
-  var getAssignmentName = document.getElementById("assignmentName");
-  var getDate = document.getElementById("date");
-  getAssignmentName.value = "";
-  getDate.value = "";
-}
+  function clear() {
+      document.getElementById("bookTitleInput").value = "";
+      document.getElementById("authorInput").value = "";
+      document.getElementById("authorInput").value = "";
+      }
+  function load() {
+    document.getElementById("bookForm").style.display === "none";
+  }
+  
+  var arr = new Array();
+  function addBook() {
+    getBook();
+    arr.push({
+      bookTitleInput: document.getElementById("bookTitleInput").value,
+      authorInput: document.getElementById("authorInput").value,
+    });
+    localStorage.setItem("Book Title", JSON.stringify(arr));
+  }
+  
+  function getBook() {
+    var str = localStorage.getItem("Book Title");
+    if (str != null) {
+      arr = JSON.parse(str);
+    }
+  }
+  function test(){
+  }
+    
+  function updateTable() {
+      getBook();
+    var displayInTable = document.getElementById("table");
+  
+    var x= displayInTable.rows.length;
+    while(--x){
+        displayInTable.deleteRow(x);
+    }
+   
+    for (i = 0; i < arr.length; i++) {
+      var title = document.getElementById("bookTitleInput").value;
+      var author = document.getElementById("authorInput").value;
+    //   var genre = document.getElementById("genreSelect").value;
+      var row = 1;
+      var newRow = displayInTable.insertRow(row);
+      var cell1 = newRow.insertCell(0);
+      var cell2 = newRow.insertCell(1);
+      var cell3 = newRow.insertCell(2);
+      cell1.innerHTML = arr[i].bookTitleInput;
+      cell2.innerHTML = arr[i].authorInput;
+      cell3.innerHTML = "<input type='checkbox' onclick='test();'>"
+    }
+  }
+  //book notes part of website 
+  
